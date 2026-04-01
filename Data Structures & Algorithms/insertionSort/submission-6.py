214114e7c334fe715b6bf467db2bf83b43d5535a
KvGroove <68174454+KvGroove@users.noreply.github.com>
2026-04-01 15:15:32 -0400

    Add: insertionSort - submission-6

---
+++ Data Structures & Algorithms/insertionSort/submission-6.py
@@ -0,0 +1,26 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    # def swap(self, a, b):
+    #     tmp = a
+    #     a = b
+    #     b = tmp
+    
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        if not pairs:
+            return pairs
+
+        answer = [pairs.copy()]
+
+        for i in range(1, len(pairs)):
+            j = i - 1
+            while j >= 0 and pairs[j + 1].key < pairs[j].key:
+                # self.swap(pairs[j], pairs[j + 1])
+                pairs[j], pairs[j + 1] = pairs[j + 1], pairs[j]
+                j -= 1
+            answer.append(pairs.copy())
+
+        return answer
