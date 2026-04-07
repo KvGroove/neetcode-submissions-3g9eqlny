61ef235bc3632aa351f58f2fafc38eedf7d28600
KvGroove <68174454+KvGroove@users.noreply.github.com>
2026-04-07 10:42:14 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:    
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        best_score = float('inf')
+
+        while left <= right:
+            k = (left + right) // 2
+
+            valid_hours = self.validHours(piles, k, h)
+            if valid_hours is False:
+                left = k + 1
+            else:
+                best_score = k
+                right = k - 1
+
+        
+        return best_score
+                
+
+    def validHours(self, piles, k, h) -> int:
+        # Returns -1 for invalid amount of hours.
+        # Returns amount of hours for valid k
+        total_hours = 0
+        for i in range(len(piles)):
+            total_hours += -(-piles[i] // k)
+        
+        if total_hours > h:
+            return False
+        else:
+            return True
