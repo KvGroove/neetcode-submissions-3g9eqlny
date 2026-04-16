5c5a4ed02f0c97188410cf279a1f214b4093db52
KvGroove <68174454+KvGroove@users.noreply.github.com>
2026-04-16 10:32:53 -0400

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+
+        s = 0
+        e = len(matrix) - 1
+
+        while s <= e:
+            mid = (s + e) // 2
+
+            if matrix[mid][0] > target:
+                e = mid - 1
+            elif matrix[mid][-1] < target:
+                s = mid + 1
+            elif matrix[mid][0] <= target and matrix[mid][-1] >= target:
+                return self.searchRow(matrix[mid], target)
+
+        return False
+
+    def searchRow(self, row, target):
+        if not row:
+            return False
+
+        s = 0
+        e = len(row) - 1    
+        
+        while s <= e:
+            mid = (s + e) // 2
+
+            if row[mid] < target:
+                s = mid + 1
+            elif row[mid] > target:
+                e = mid - 1
+            elif row[mid] == target:
+                return True
+        
+        return False
+
+     
