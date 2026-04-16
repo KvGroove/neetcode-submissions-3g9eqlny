c4717e89928f97938204ba59e84b3cbec52ddd93
KvGroove <68174454+KvGroove@users.noreply.github.com>
2026-04-16 10:01:49 -0400

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        return self.bstHelper(nums, target, 0, len(nums) - 1)
+
+    def bstHelper(self, nums, target, s, e) -> int:
+        if s > e:
+            return -1
+            
+        mid = (s + e) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return self.bstHelper(nums, target, mid + 1, e)
+        else:
+            return self.bstHelper(nums, target, s, mid - 1)
+
+
+# nums = [-1,0,2,4,6,8], target = 4
+
+# bstHelper(s = 0, e = 5)
+# mid = 2, num[mid] = 2 < target
+# s = 3, e = 5
+
+# bstHelper(s = 3, e = 5) 
+# mid = 4, num[mid] = 6 > target
+# s = 3, e = 3
+
+# bstHelper(s = 3, e = 3)
+# mid = 3, num[3] = 4 == target
+# return mid
+
